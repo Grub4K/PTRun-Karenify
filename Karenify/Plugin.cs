94e9f604142c6826606bdf782d88d85b71998f08
Simon Sawicki <contact@grub4k.xyz>
2023-10-10 19:36:24 +0200

    Initial commit

---
+++ Karenify/Plugin.cs
@@ -0,0 +1,83 @@
+using ManagedCommon;
+using System.Windows.Forms;
+using Wox.Plugin;
+
+namespace Community.Powertoys.Run.Plugin.Karenify;
+
+public class Plugin : IPlugin
+{
+    public string Name => "Karenify";
+    public string Description => "Karenify input and copy it to the clipboard";
+
+    private PluginInitContext? _context;
+    private string? _iconPath;
+
+    public void Init(PluginInitContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        _context = context;
+        _context.API.ThemeChanged += (_, theme) => UpdateIconPath(theme);
+        UpdateIconPath(_context.API.GetCurrentTheme());
+    }
+
+    public List<Result> Query(Query query)
+    {
+        ArgumentNullException.ThrowIfNull(query);
+        if (string.IsNullOrEmpty(query.Search))
+            return new();
+
+        var options = new KarenifyOptions[] {
+            new(UppercaseFirst: true),
+            new(UppercaseFirst: false),
+        };
+        return options
+            .Select(options => {
+                var result = Karenify(query.Search, options);
+                return new Result()
+                {
+                    QueryTextDisplay = query.Search,
+                    IcoPath = _iconPath,
+                    Title = result,
+                    SubTitle = Name,
+                    Action = _ => {
+                        Clipboard.SetText(result);
+                        return true;
+                    },
+                };
+            })
+            .ToList();
+    }
+
+    private string Karenify(string source, KarenifyOptions options)
+    {
+        var upper = options.UppercaseFirst;
+        var buffer = new char[source.Length];
+
+        for (var i = 0; i < source.Length; i++)
+        {
+            if (!Char.IsLetter(source[i]))
+            {
+                buffer[i] = source[i];
+            }
+            else
+            {
+                buffer[i] = upper
+                    ? Char.ToUpper(source[i])
+                    : Char.ToLower(source[i]);
+                upper = !upper;
+            }
+        }
+
+        return new(buffer);
+    }
+
+    private void UpdateIconPath(Theme theme)
+    {
+        _iconPath = theme is Theme.Light or Theme.HighContrastWhite
+            ? "res/Karenify.light.png"
+            : "res/Karenify.dark.png";
+    }
+}
+
+public record struct KarenifyOptions(bool UppercaseFirst);
---
+++ README.md
@@ -0,0 +1,33 @@
+# PowerToys Run: Karenify Plugin
+
+Do you wish you were a middle-aged white mom with a [hip, modern haircut](https://www.hottesthaircuts.com/wp-content/uploads/2020/10/Karen-Haircut-10.jpg), but you just can't find the manager?
+
+ThEn ThIs Is ThE pLuGiN fOr YoU!
+
+## Requirements
+
+- PowerToys (tested with v0.74.1)
+
+## Installation
+
+Install:
+1. Download the [latest release](https://github.com/Grub4K/PTRun-Karenify/releases/latest)
+2. Close PowerToys
+3. Extract the `Karenify` folder inside it to `%LocalAppData%\Microsoft\PowerToys\PowerToys Run\Plugins`
+4. Start PowerToys
+
+### Usage
+
+1. Open PowerToys Run (default: `<Alt>` + `<Space>`)
+2. Type the direct activation command (default: `k`) and a space
+3. Continue typing whatever you want to karenify
+4. Select one of the entries, then press `<Enter>` to copy that into clipboard
+
+## Building
+
+1. Clone this repository
+2. Copy the following files from `%PROGRAMFILES%\PowerToys\modules\launcher` to the `Karenify/lib/` directory:
+	- `PowerToys.Common.UI.dll`
+	- `PowerToys.ManagedCommon.dll`
+	- `Wox.Plugin.dll`
+3. Build the project with `dotnet publish -c:Release`, or equivalent
